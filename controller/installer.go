5c805af8f5d8c26dddb20cf09dad359b296b68aa
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-07-29 18:41:49 +0530

    update installer

--- controller/installer.go
+++ controller/installer.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 
 	"github.com/anik-ghosh-au7/go-pack-node/utils"
 )
@@ -22,54 +21,6 @@ type Dist struct {
 type PackageVersionInfo struct {
 	Version string `json:"version"`
 	Dist    Dist   `json:"dist"`
-}
-
-func Install(args ...string) {
-	baseDir, _ := os.Getwd() // Get the current working directory
-	var wg sync.WaitGroup    // Create a WaitGroup
-
-	if len(args) == 0 {
-		// If no args are provided, install all dependencies from dependencies.json
-		// ... (implementation omitted for brevity) ...
-	} else {
-		for _, arg := range args {
-			wg.Add(1) // Add a count to the WaitGroup for each argument
-
-			go func(arg string) { // Wrap the logic for each argument in a goroutine
-				defer wg.Done() // Decrement the WaitGroup count when the goroutine finishes
-
-				packageAndVersion := strings.Split(arg, "@")
-				packageName := packageAndVersion[0]
-				packageVersion := "latest"
-				if len(packageAndVersion) > 1 {
-					packageVersion = packageAndVersion[1]
-				}
-
-				// Fetch package info from npm registry
-				packageInfo, err := FetchPackageInfo(packageName, packageVersion)
-				if err != nil {
-					fmt.Println("Error fetching package info:", err)
-					return
-				}
-
-				// Check if the version exists in the cache
-				cacheDir := filepath.Join(baseDir, ".cache", fmt.Sprintf("%s@%s", packageName, packageInfo.Version))
-				if !utils.DirExists(cacheDir) {
-					// If not, download it and save it in the cache
-					err := DownloadPackage(packageInfo, cacheDir)
-					if err != nil {
-						fmt.Println("Error downloading package:", err)
-						return
-					}
-				}
-
-				// Update dependencies.json and dependencies-lock.json
-				// ... (implementation omitted for brevity) ...
-			}(arg)
-		}
-	}
-
-	wg.Wait() // Wait for all goroutines to finish
 }
 
 func FetchPackageInfo(packageName string, version string) (*PackageVersionInfo, error) {
@@ -95,6 +46,45 @@ func FetchPackageInfo(packageName string, version string) (*PackageVersionInfo, 
 	}
 
 	return &packageInfo, nil
+}
+
+func Install(args ...string) {
+	baseDir, _ := os.Getwd() // Get the current working directory
+
+	if len(args) == 0 {
+		// If no args are provided, install all dependencies from dependencies.json
+		// ... (implementation omitted for brevity) ...
+	} else {
+		for _, arg := range args {
+			packageAndVersion := strings.Split(arg, "@")
+			packageName := packageAndVersion[0]
+			packageVersion := "latest"
+			if len(packageAndVersion) > 1 {
+				packageVersion = packageAndVersion[1]
+			}
+
+			// Fetch package info from npm registry
+			packageInfo, err := FetchPackageInfo(packageName, packageVersion)
+			if err != nil {
+				fmt.Println("Error fetching package info:", err)
+				continue
+			}
+
+			// Check if the version exists in the cache
+			cacheDir := filepath.Join(baseDir, ".cache", packageName, packageInfo.Version)
+			if !utils.DirExists(cacheDir) {
+				// If not, download it and save it in the cache
+				err := DownloadPackage(packageInfo, cacheDir)
+				if err != nil {
+					fmt.Println("Error downloading package:", err)
+					continue
+				}
+			}
+
+			// Update dependencies.json and dependencies-lock.json
+			// ... (implementation omitted for brevity) ...
+		}
+	}
 }
 
 func DownloadPackage(packageInfo *PackageVersionInfo, destination string) error {
@@ -136,19 +126,19 @@ func DownloadPackage(packageInfo *PackageVersionInfo, destination string) error 
 
 		// Create the directories in the path
 		if hdr.Typeflag == tar.TypeDir {
-			if err := os.MkdirAll(filepath.Join(destination, hdr.Name), 0755); err != nil {
+			if err := os.MkdirAll(filepath.Join(destination, strings.TrimPrefix(hdr.Name, "package/")), 0755); err != nil {
 				return err
 			}
 		} else {
 			// Create the directory path
-			dir := filepath.Join(destination, filepath.Dir(hdr.Name))
+			dir := filepath.Join(destination, filepath.Dir(strings.TrimPrefix(hdr.Name, "package/")))
 			err := os.MkdirAll(dir, 0755)
 			if err != nil {
 				return err
 			}
 
 			// Create the file
-			outFile, err := os.Create(filepath.Join(destination, hdr.Name))
+			outFile, err := os.Create(filepath.Join(destination, strings.TrimPrefix(hdr.Name, "package/")))
 			if err != nil {
 				return err
 			}
