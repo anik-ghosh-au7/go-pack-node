d17f783721a0e504547054c35b0a82fa014648ba
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-07-30 09:56:41 +0530

    update schema

--- schema/package.go
+++ schema/package.go
@@ -7,5 +7,5 @@ type Dist struct {
 type PackageVersionInfo struct {
 	Version      string            `json:"version"`
 	Dist         Dist              `json:"dist"`
-	Dependencies map[string]string `json:"dependencies"` // Add this field
+	Dependencies map[string]string `json:"dependencies"`
 }
